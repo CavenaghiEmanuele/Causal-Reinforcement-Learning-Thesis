ddfa7ceae4b77762143163e02876ee76b45aad9c
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2021-03-17 21:18:25 +0100

    Add medical_treatment environment

--- reilly/environments/__init__.py
+++ reilly/environments/__init__.py
@@ -3,6 +3,7 @@ from .hierarchical_environment import HierarchicalEnvironment
 from .causal_environment import CausalEnvironment
 from .gym import *
 from .text import TextEnvironment, TextNeighbor
+from .medical_treatment import MedicalTreatment
 
 
 def ENVIRONMENTS():
