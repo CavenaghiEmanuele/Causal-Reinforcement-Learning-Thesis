45a4b6edd82fc53047f036589ab5b85afed35894
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2021-04-08 17:36:46 +0200

    Add plot function

--- reilly/utils/__init__.py
+++ reilly/utils/__init__.py
@@ -1,2 +1,2 @@
-from .plot import plot, plot_mean
+from .plot import plot, plot_mean, plot_results
 from .causal_inference import *
--- reilly/utils/plot.py
+++ reilly/utils/plot.py
@@ -1,5 +1,9 @@
+import typing
+import reilly as rl
+
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
 
 def plot(data: pd.DataFrame):
@@ -18,6 +22,36 @@ def plot(data: pd.DataFrame):
         plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
     plt.legend(loc='upper left')
     plt.show()
+
+
+def plot_results(data: pd.DataFrame):
+    data = data.drop('step', axis=1).\
+        groupby(['test', 'sample', 'agent']).sum()
+    data = data.groupby(['test', 'agent']).mean()
+    tests = data.columns.values.tolist()
+    data = data.groupby('agent')
+    for test in tests:
+        agents = data[test].apply(list).to_dict()
+        plt.figure(test)
+        unique_legend = True
+        for agent in agents:
+            label = re.search('Params: (.*)', agent).group(1)
+            if 'Random' in label:
+                plt.plot(agents[agent], 'k', label=label)
+            else:
+                if unique_legend:
+                    plt.plot(agents[agent], 'r', label=label)
+                else:
+                    plt.plot(agents[agent], 'r')
+                unique_legend = False
+
+        plt.ylabel(test)
+        plt.xlabel('Number of tests')
+        plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
+
+    plt.legend(loc='upper left')
+    plt.show()
+
 
 def plot_mean(data: pd.DataFrame, n:int):
     data = data.drop('step', axis=1).\
