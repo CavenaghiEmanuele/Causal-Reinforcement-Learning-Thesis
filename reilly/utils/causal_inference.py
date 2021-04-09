5f08194b49da53dfe972a8365a1ea2020a60c41e
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2021-04-09 19:06:36 +0200

    Update causal inference function

--- reilly/utils/causal_inference.py
+++ reilly/utils/causal_inference.py
@@ -7,24 +7,21 @@ from typing import Dict, List
 Apply the adjustment formula
 evidences is the state of the environment
 '''
-def causal_query(target:str, evidence:Dict, actions:List[str], model:BayesianModel):
-
+def causal_query(target:str, evidence:Dict, actions:Dict, model:BayesianModel):
     queries = {}
     #Pearl Adjustment formula
-    for action in actions:
+    for action in actions.keys():
         z = set(model.get_parents(action))
 
         inference_engine = VariableElimination(model)
-        base_query = inference_engine.query(variables=[target], evidence={**evidence, **{action:'True'}}, show_progress=False)
-
+        base_query = inference_engine.query(variables=[target, action], evidence={**evidence}, show_progress=False)
         if len(z) != 0:
             adjustment_query = inference_engine.query(variables=z, show_progress=False)
             query = base_query.product(adjustment_query, inplace=False)
         else:
             query = base_query
-
+        
         query.normalize()
         query.marginalize(z)
         queries.update({action : query})
-
     return queries
