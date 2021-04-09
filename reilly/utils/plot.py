7cb457e08150633335426074ff5586722d023e2e
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2021-04-09 19:06:48 +0200

    Add causal agent to plot function

--- reilly/utils/plot.py
+++ reilly/utils/plot.py
@@ -1,6 +1,5 @@
-import typing
-import reilly as rl
-
+from networkx.generators.triads import TRIAD_EDGES
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
@@ -33,11 +32,18 @@ def plot_medical_treatment_results(data: pd.DataFrame, title: str):
     for test in tests:
         agents = data[test].apply(list).to_dict()
         plt.figure(title)
+        unique_legend_causal = True
         unique_legend = True
         for agent in agents:
-            label = re.search('Params: (.*)', agent).group(1)
+            label = re.search('Params: ([A-Z.,a-z]*)', agent).group(1)
             if 'Random' in label:
-                plt.plot(agents[agent], 'k', label=label)
+                plt.plot(agents[agent], 'k', label=label, linewidth=4)
+            elif 'CausalQLearning' in label:
+                if unique_legend_causal:
+                    plt.plot(agents[agent], 'b', label=label)
+                else:
+                    plt.plot(agents[agent], 'b')
+                unique_legend_causal = False
             else:
                 if unique_legend:
                     plt.plot(agents[agent], 'r', label=label)
@@ -45,12 +51,22 @@ def plot_medical_treatment_results(data: pd.DataFrame, title: str):
                     plt.plot(agents[agent], 'r')
                 unique_legend = False
 
-        plt.ylabel('Cumulative Reward')
-        plt.xlabel('Number of tests')
-        plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
+    #plt.title(title, fontsize=30)
+
+    plt.tick_params(axis='both', which='major', labelsize=24)
+
+    plt.ylabel('Cumulative Reward', fontsize=26)
+    plt.ylim([42.5, 92.5])
+    plt.yticks(np.arange(42.5, 92.5+5, 5.0))
     
-    plt.title(title)
-    plt.legend(loc='upper left')
+    plt.xlabel('Number of tests', fontsize=26)
+    plt.xlim([0, 9])
+    plt.xticks(np.arange(0, 9+1, 1.0))
+    
+    plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
+    plt.legend(loc='upper left', fancybox=True, shadow=True, prop={'size': 28})
+    plt.subplots_adjust(left=0.07, right=0.98, top=0.95, bottom=0.10)
+       
     plt.show()
 
 
