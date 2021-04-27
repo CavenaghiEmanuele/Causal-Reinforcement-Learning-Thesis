c7845fb8f2c7fbc018a7a6796fb018f685b5a4d3
CavenaghiEmanuele <cavenaghi.emanuele@gmail.com>
2021-04-27 18:50:33 +0200

    Add plot function for taxi env

--- reilly/utils/plot.py
+++ reilly/utils/plot.py
@@ -89,3 +89,55 @@ def plot_mean(data: pd.DataFrame, n:int):
         plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
     plt.legend(loc='upper left')
     plt.show()
+
+
+def plot_taxi_results(data: pd.DataFrame, title: str):
+    data = data.drop('step', axis=1).\
+        groupby(['test', 'sample', 'agent']).sum()
+    data = data.groupby(['test', 'agent']).mean()
+    tests = data.columns.values.tolist()
+    data = data.groupby('agent')
+    for test in tests:
+        agents = data[test].apply(list).to_dict()
+        plt.figure(test)
+        unique_legend_causal = True
+        unique_legend = True
+        for agent in agents:
+            label = re.search('Params: ([A-Z.,a-z]*)', agent).group(1)
+            if 'Random' in label:
+                plt.plot(agents[agent], 'k', label=label, linewidth=4)
+            elif 'CausalQLearning' in label:
+                if unique_legend_causal:
+                    plt.plot(agents[agent], 'b', label=label)
+                else:
+                    plt.plot(agents[agent], 'b')
+                unique_legend_causal = False
+            else:
+                if unique_legend:
+                    plt.plot(agents[agent], 'r', label=label)
+                else:
+                    plt.plot(agents[agent], 'r')
+                unique_legend = False
+
+        plt.tick_params(axis='both', which='major', labelsize=24)
+
+        plt.xlabel('Number of tests', fontsize=26)
+        plt.xlim([0, 20])
+        plt.xticks(np.arange(0, 20+1, 1.0))
+
+        plt.grid(linestyle='--', linewidth=0.5, color='.25', zorder=-10)
+        plt.legend(loc='upper left', fancybox=True, shadow=True, prop={'size': 28})
+        plt.subplots_adjust(left=0.07, right=0.98, top=0.95, bottom=0.10)
+
+        if test == 'wins':
+            plt.ylabel('Wins', fontsize=26)
+            plt.ylim([0.0, 1.0])
+            plt.yticks(np.arange(0-0.1, 1+0.1, 0.05))
+        else:
+            plt.ylabel('Cumulative Reward', fontsize=26)
+            plt.ylim([-2000, 20])
+            plt.yticks(np.arange(-2000, 20, 100.0))
+        
+
+       
+    plt.show()
